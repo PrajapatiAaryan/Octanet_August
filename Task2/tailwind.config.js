66057cf600b69d1a9da8d6d3b0d46d8d3fa01b09
PrajapatiAaryan <prajapatiaryan306@gmail.com>
2024-08-12 19:17:06 +0530

    Add files via upload

---
+++ Task2/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'hero-pattern': "url('./assets/bg1.jpg')",
+        'hero-pattern2': "url('./assets/bg2.webp')",
+      }
+    },
+  },
+  plugins: [],
+}
