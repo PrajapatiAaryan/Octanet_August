e027d03bcff6139e64917db53015e7e010326b58
PrajapatiAaryan <prajapatiaryan306@gmail.com>
2024-08-13 19:08:43 +0530

    Update Todotask.jsx

--- Task2/src/components/Todotask.jsx
+++ Task2/src/components/Todotask.jsx
@@ -2,79 +2,74 @@ import React, { useEffect, useState } from "react";
 import Navbar from "./Navbar";
 
 const Todotask = () => {
- const [todos, settodos] = useState([
-    { id: 1, todo: "this is demo todo", iscompleted: false },
+  const [todos, settodos] = useState([
+    { id: Date.now(), todo: "this is a demo todo task", iscompleted: false },
   ]);
   const [task, settask] = useState("");
-  const [completed, setcompleted] = useState(true)
-//add function
+  const [completed, setcompleted] = useState(true);
+  useEffect(() => {
+    let todolist = localStorage.getItem("todolist");
+    // let newtodolist = JSON.parse(todolist);
+    console.log(JSON.parse(todolist) ,todos?.length )
+    let newtodolist = todos?.length === 0 ? [] : JSON.parse(todolist) ;
+    console.log(newtodolist)
+    
+      settodos(newtodolist);
+  }, []);
+  //add function
   const handleadd = () => {
     let updatedtodo = [
       ...todos,
       { id: Date.now(), todo: task, iscompleted: false },
     ];
     settodos(updatedtodo);
-    console.log("1" ,updatedtodo)
     localStorage.setItem("todolist", JSON.stringify(updatedtodo));
     settask("");
   };
-//cheakbox function  
+  //cheakbox function
   const handlecheak = (id) => {
     let updatedtodo = todos.map((item) =>
       item.id == id ? { ...item, iscompleted: !item.iscompleted } : item
     );
     settodos(updatedtodo);
-    console.log("1" ,updatedtodo)
     localStorage.setItem("todolist", JSON.stringify(updatedtodo));
   };
 
-//edit funcation
+  //edit funcation
   const handleedit = (id, todo) => {
     settask(todo);
     let updatedtodo = todos.filter((item) => item.id != id);
     localStorage.setItem("todolist", JSON.stringify(updatedtodo));
     settodos(updatedtodo);
-    console.log("1" ,updatedtodo)
   };
-//delete function  
+  //delete function
   const handledelete = (id) => {
     let updatedtodo = todos.filter((item) => item.id != id);
     localStorage.setItem("todolist", JSON.stringify(updatedtodo));
     settodos(updatedtodo);
-    console.log("1" ,updatedtodo)
   };
-//show all function
-   const togglechange = () => {
-     setcompleted(!completed)
-     let todolist = localStorage.getItem("todolist");
-     let newtodos = JSON.parse(todolist)
-     let updatedtodo = completed?newtodos.filter((item)=>item.iscompleted==false):newtodos
-     console.log("1" ,updatedtodo)
-     settodos(updatedtodo)
-   }
-     
-   useEffect(() => {
+  //show all function
+  const togglechange = () => {
+    setcompleted(!completed);
     let todolist = localStorage.getItem("todolist");
-    let newtodolist = JSON.parse(todolist)
-    if (newtodolist.length===0) {
-      console.log("hekki i am render if" , todos  , JSON.parse(todolist).length);
-    }
-    else {
-      console.log("hekki i am render else" , todos  , JSON.parse(todolist).length);
-      settodos(JSON.parse(todolist))
-    }
-
-  }, []);
+    let newtodos = JSON.parse(todolist);
+    let updatedtodo = completed
+      ? newtodos.filter((item) => item.iscompleted == false)
+      : newtodos;
+    settodos(updatedtodo);
+  };
 
   return (
     <>
       <div className="">
-       
         <div className="flex justify-center items-center flex-col pt-12">
           <h1 className="text-black text-[2rem] px-9 pt-7 pb-5">
             Enter Your Task here
           </h1>
-          <form className=" w-full lg:w-[50%] flex gap-2 justify-center items-center" onSubmit={handleadd}>
+          <form
+            className=" w-full lg:w-[50%] flex gap-2 justify-center items-center"
+            onSubmit={handleadd}
+          >
             <input
               className="w-[80%] lg:ml-8 mx-2 px-2 py-3 text-[1.2rem] bg-transparent text-black"
               type="text"
@@ -85,7 +80,7 @@ const Todotask = () => {
             <button
               onClick={handleadd}
               disabled={task.length < 2}
-              className="border border-black px-4 py-3  bg-gradient-to-t from-gray-500 to-gray-300 rounded-xl shadow-md shadow-gray-800"
+              className="border border-black px-4 py-3  bg-gradient-to-tr from-purple-500 to-violet-500 rounded-xl shadow-md shadow-gray-800"
             >
               Add
             </button>
@@ -94,43 +89,54 @@ const Todotask = () => {
             <h1 className="text-xl px-3 py-2 flex justify-center items-center font-bold">
               Here Your All Task List
             </h1>
-            {todos?.length==0?(<>
-            <h1  className="text-lg px-3 py-2 flex justify-center items-center">NO Task is pending</h1>
-            </>): <div  className="flex justify-start border-b border-black items-center text-xl gap-4 px-2 py-2">
-              <input type="checkbox" className="text-5xl" checked ={completed} onChange={()=>togglechange()} />
-              <h1>Show all</h1>
-            </div>}
+            {todos?.length == 0 ? (
+              <>
+                <h1 className="text-lg px-3 py-2 flex justify-center items-center">
+                  NO Task is pending
+                </h1>
+              </>
+            ) : (
+              <div className="flex justify-start border-b border-black items-center text-xl gap-4 px-2 py-2">
+                <input
+                  type="checkbox"
+                  className="text-5xl"
+                  checked={completed}
+                  onChange={() => togglechange()}
+                />
+                <h1>Show all</h1>
+              </div>
+            )}
             {todos?.map((item) => (
-                <div key={item.id} className=" w-[90vw] lg:w-[45vw] ">
-                  <div className="flex justify-between px-2 py-2 items-center border-b">
-                    <div className="flex gap-4 text-xl font-semibold">
-                      <input
-                        className=""
-                        type="checkbox"
-                        checked={item.iscompleted}
-                        onChange={() => handlecheak(item.id)}
-                      />
-                      <h1 className={item.iscompleted ? "line-through" : ""}>
-                        {item.todo}
-                      </h1>
-                    </div>
-                    <div className="flex justify-center items-center gap-2">
-                      <button
-                        className=" bg-gradient-to-t from-gray-500 to-gray-300 px-4 py-2 flex justify-center items-center border border-gray-800 rounded-xl shadow-purple-500 shadow-sm"
-                        onClick={() => handleedit(item.id, item.todo)}
-                      >
-                        Edit
-                      </button>
+              <div key={item.id} className=" w-[90vw] lg:w-[45vw] ">
+                <div className="flex justify-between px-2 py-2 items-center border-b">
+                  <div className="flex gap-4 text-xl font-semibold">
+                    <input
+                      className=""
+                      type="checkbox"
+                      checked={item.iscompleted}
+                      onChange={() => handlecheak(item.id)}
+                    />
+                    <h1 className={item.iscompleted ? "line-through" : ""}>
+                      {item.todo}
+                    </h1>
+                  </div>
+                  <div className="flex justify-center items-center gap-2">
+                    <button
+                      className=" bg-gradient-to-t from-gray-500 to-gray-300 px-4 py-2 flex justify-center items-center border border-gray-800 rounded-xl shadow-purple-500 shadow-sm"
+                      onClick={() => handleedit(item.id, item.todo)}
+                    >
+                      Edit
+                    </button>
 
-                      <button
-                        className=" bg-gradient-to-t from-gray-500 to-gray-300 px-4 py-2 flex justify-center items-center border border-gray-800 rounded-xl shadow-purple-500 shadow-sm"
-                        onClick={() => handledelete(item.id)}
-                      >
-                        Delete
-                      </button>
-                    </div>
+                    <button
+                      className=" bg-gradient-to-t from-gray-500 to-gray-300 px-4 py-2 flex justify-center items-center border border-gray-800 rounded-xl shadow-purple-500 shadow-sm"
+                      onClick={() => handledelete(item.id)}
+                    >
+                      Delete
+                    </button>
                   </div>
                 </div>
+              </div>
             ))}
           </div>
         </div>
